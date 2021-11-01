89b5fe665b51bc20880c56825a175f78edd58630
Cameron Hart <cameron.hart@gmail.com>
2021-11-01 23:32:20 +1300

    Add enemy which tracks the player ship.
    TODO: apply a rotation speed limit to enemy.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use bevy::{core::FixedTimestep, prelude::*};
+use bevy::{core::FixedTimestep, math::const_vec2, prelude::*};
 
 const TIME_STEP: f32 = 1.0 / 60.0;
+const BOUNDS: Vec2 = const_vec2!([1200.0, 640.0]);
 
 fn main() {
     App::new()
@@ -43,6 +44,7 @@ fn setup(
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.add(ship_handle.into()),
+            transform: Transform::from_xyz(0.0, 40.0 - BOUNDS.y / 2.0, 0.0),
             ..Default::default()
         })
         .insert(ShipConfig {
@@ -57,49 +59,43 @@ fn setup(
             material: materials.add(enemy_handle.into()),
             ..Default::default()
         })
-    .insert(ShipConfig {
-        movement_speed: 0.0,
-        rotation_speed: f32::to_radians(180.0)
-    });
+        .insert(ShipConfig {
+            movement_speed: 0.0,
+            rotation_speed: f32::to_radians(180.0),
+        });
 
     // Add walls
     let wall_material = materials.add(Color::rgb(0.8, 0.8, 0.8).into());
     let wall_thickness = 10.0;
-    let bounds = Vec2::new(900.0, 600.0);
 
     // left
-    commands
-        .spawn_bundle(SpriteBundle {
-            material: wall_material.clone(),
-            transform: Transform::from_xyz(-bounds.x / 2.0, 0.0, 0.0),
-            sprite: Sprite::new(Vec2::new(wall_thickness, bounds.y + wall_thickness)),
-            ..Default::default()
-        });
+    commands.spawn_bundle(SpriteBundle {
+        material: wall_material.clone(),
+        transform: Transform::from_xyz(-BOUNDS.x / 2.0, 0.0, 0.0),
+        sprite: Sprite::new(Vec2::new(wall_thickness, BOUNDS.y + wall_thickness)),
+        ..Default::default()
+    });
     // right
-    commands
-        .spawn_bundle(SpriteBundle {
-            material: wall_material.clone(),
-            transform: Transform::from_xyz(bounds.x / 2.0, 0.0, 0.0),
-            sprite: Sprite::new(Vec2::new(wall_thickness, bounds.y + wall_thickness)),
-            ..Default::default()
-        });
+    commands.spawn_bundle(SpriteBundle {
+        material: wall_material.clone(),
+        transform: Transform::from_xyz(BOUNDS.x / 2.0, 0.0, 0.0),
+        sprite: Sprite::new(Vec2::new(wall_thickness, BOUNDS.y + wall_thickness)),
+        ..Default::default()
+    });
     // bottom
-    commands
-        .spawn_bundle(SpriteBundle {
-            material: wall_material.clone(),
-            transform: Transform::from_xyz(0.0, -bounds.y / 2.0, 0.0),
-            sprite: Sprite::new(Vec2::new(bounds.x + wall_thickness, wall_thickness)),
-            ..Default::default()
-        });
+    commands.spawn_bundle(SpriteBundle {
+        material: wall_material.clone(),
+        transform: Transform::from_xyz(0.0, -BOUNDS.y / 2.0, 0.0),
+        sprite: Sprite::new(Vec2::new(BOUNDS.x + wall_thickness, wall_thickness)),
+        ..Default::default()
+    });
     // top
-    commands
-        .spawn_bundle(SpriteBundle {
-            material: wall_material,
-            transform: Transform::from_xyz(0.0, bounds.y / 2.0, 0.0),
-            sprite: Sprite::new(Vec2::new(bounds.x + wall_thickness, wall_thickness)),
-            ..Default::default()
-        });
-
+    commands.spawn_bundle(SpriteBundle {
+        material: wall_material,
+        transform: Transform::from_xyz(0.0, BOUNDS.y / 2.0, 0.0),
+        sprite: Sprite::new(Vec2::new(BOUNDS.x + wall_thickness, wall_thickness)),
+        ..Default::default()
+    });
 }
 
 fn player_movement_system(
@@ -108,45 +104,57 @@ fn player_movement_system(
 ) {
     let (ship, mut transform) = query.single_mut();
 
-    let mut rotation_direction = 0.0;
-    let mut movement_direction = 0.0;
+    let mut rotation_factor = 0.0;
+    let mut movement_factor = 0.0;
 
     if keyboard_input.pressed(KeyCode::Left) {
-        rotation_direction += 1.0;
+        rotation_factor += 1.0;
     }
 
     if keyboard_input.pressed(KeyCode::Right) {
-        rotation_direction -= 1.0;
+        rotation_factor -= 1.0;
     }
 
     if keyboard_input.pressed(KeyCode::Up) {
-        movement_direction += 1.0;
+        movement_factor += 1.0;
     }
 
     // create the change in rotation around the Z axis (pointing through the 2d plane of the screen)
-    let rotation_delta =
-        Quat::from_rotation_z(rotation_direction * ship.rotation_speed * TIME_STEP);
+    let rotation_delta = Quat::from_rotation_z(rotation_factor * ship.rotation_speed * TIME_STEP);
     // update the ship rotation with our rotation delta
     transform.rotation *= rotation_delta;
 
-    // create the change in translation using the new rotation directon
-    let translation_delta =
-        transform.rotation * Vec3::Y * movement_direction * ship.movement_speed * TIME_STEP;
+    // get the ship's forward vector by apply the current rotation to the ships initial facing vector
+    let movement_direction = transform.rotation * Vec3::Y;
+    // get the distance the ship will move based on direction, the ship's movement speed and delta
+    // time
+    let movement_distance = movement_factor * ship.movement_speed * TIME_STEP;
+    // create the change in translation using the new movement direction and distance
+    let translation_delta = movement_direction * movement_distance;
     // update the ship translation with our new translation delta
     transform.translation += translation_delta;
 
     // bound the ship within the walls
-    let extents = Vec3::new(450.0, 300.0, 0.0);
+    let extents = Vec3::from((BOUNDS / 2.0, 0.0));
     transform.translation = transform.translation.min(extents).max(-extents);
 }
 
 fn enemy_movement_system(
     mut query: Query<(&ShipConfig, &mut Transform), Without<Player>>,
-    player_query: Query<&Transform, With<Player>>
+    player_query: Query<&Transform, With<Player>>,
 ) {
-    let _player_transform = player_query.single();
+    let player_transform = player_query.single();
 
-    for (_enemy_ship, mut _enemy_transform) in query.iter_mut() {
-        // TODO
+    for (_enemy_ship, mut enemy_transform) in query.iter_mut() {
+        let enemy_forward = enemy_transform.rotation * Vec3::Y;
+        let player_direction =
+            (player_transform.translation - enemy_transform.translation).normalize();
+
+        // get the quaternion to rotate from the current enemy facing direction to the direction
+        // facing the player
+        let rotate_to_player = Quat::from_rotation_arc(enemy_forward, player_direction);
+
+        // rotate the enemy to face the player
+        enemy_transform.rotation *= rotate_to_player;
     }
 }
